1be6f2faca3e93610ae34a05cbd9f9f793cd6fa4
Max Morrison <maxrmorrison@gmail.com>
2022-10-10 16:28:05 +0000

    LibriTTS speakers

--- emphases/load.py
+++ emphases/load.py
@@ -1,11 +1,29 @@
 import json
 
+import torchaudio
+
 import emphases
 
 
 ###############################################################################
 # Loading utilities
 ###############################################################################
+
+
+def audio(file):
+    """Load audio and maybe resample"""
+    # Load
+    audio, sample_rate = torchaudio.load(file)
+
+    # Maybe resample
+    if sample_rate != emphases.SAMPLE_RATE:
+        resampler = torchaudio.transforms.Resample(
+            sample_rate,
+            emphases.SAMPLE_RATE)
+        audio = resampler(audio)
+
+    return audio
+
 
 def partition(dataset):
     """Load partitions for dataset"""
